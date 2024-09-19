6a27e5ebc4286fd787aadfcb82d08668c1ebb1b4
Christoffer <158524990+Christoffer-Mesquita@users.noreply.github.com>
2024-09-19 20:22:06 -0300

    Update Logic.java

--- Game/src/game/logic/Logic.java
+++ Game/src/game/logic/Logic.java
@@ -1,5 +1,6 @@
 package game.logic;
 
+import game.functions.functions;
 import game.struct.Gurillo;
 
 public class Logic{
@@ -7,6 +8,8 @@ public class Logic{
 
 public static void main(String arg[]) {
 	Gurillo gur = new Gurillo();
+	functions fun = new functions();
+	
 	gur.teste();
 	System.out.println("a");
 	}
