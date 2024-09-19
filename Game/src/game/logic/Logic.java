77e699a44433604c0db14b02e0c144067d260852
Zyon-Nahoy <zyonnahoy@gmail.com>
2024-09-19 20:16:58 -0300

    mostrando como vai funcionar

---
+++ Game/src/game/logic/Logic.java
@@ -0,0 +1,12 @@
+package game.logic;
+
+import game.struct.Gurillo;
+
+public class Logic{
+	
+
+public static void main(String arg[]) {
+	Gurillo gur = new Gurillo();
+	gur.teste();
+	}
+}
---
+++ Game/src/game/struct/Gurillo.java
@@ -0,0 +1,9 @@
+package game.struct;
+
+public class Gurillo{
+	
+	public String teste() {
+		return "HALLO";
+	}
+	
+}
