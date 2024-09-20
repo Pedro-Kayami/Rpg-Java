b085e26d014a737dd3067b41486d01c9cbedeb7c
Christoffer <158524990+Christoffer-Mesquita@users.noreply.github.com>
2024-09-19 21:32:24 -0300

    Update functions.java

--- Game/src/game/functions/functions.java
+++ Game/src/game/functions/functions.java
@@ -1,8 +1,11 @@
 package game.functions;
 
 public class functions {
-	int saude;
+	int saude = 100;
+	int forca = 1;
 	
+	
+	// SISTEMA DE SAUDE
 	public int puxarSaude() {
 		return saude;
 	}
@@ -11,11 +14,32 @@ public class functions {
 		this.saude = Math.max(0,  saude);
 	}
 	
-	public void reduzirSaude(int dano) {
+	public void dano(int dano) {
 		setarSaude(saude - dano);
 	}
 	
 	public void adicionarSaude(int adcSaude) {
 		setarSaude(saude + adcSaude);
 	}
+	
+	
+	
+	// SISTEMA DE FORÇA 
+	public int puxarForca() {
+		return forca;
+	}
+	
+	public void adicionarForca(int adcForca) {
+		this.forca = (forca + adcForca);
+	}
+	
+	public void removerForca(int rmForca) {
+		this.forca = (forca - rmForca);
+	}
+	
+	public void resetarForca() {
+		forca = 1;
+	}
+	
+	
 }
