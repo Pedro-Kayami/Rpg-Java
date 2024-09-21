add3b06a808526306f907c43242665d5114087ab
Zyon-Nahoy <zyonnahoy@gmail.com>
2024-09-20 21:03:33 -0300

    inicio da Historia

--- Game/src/historia/package-info.java
+++ Game/src/historia/package-info.java
@@ -1,1 +1,8 @@
 package historia;
+// Contexto geral - essa historia começa com um mundo em guerra onde tudo esta destruido e só existe uma pesoa que pode nos salvar Gurilo
+//Tudo começa com nosso querido Gurilo indo atraz do diplominos o unico item capas de acabar com a terrivel guerra o Diplominus esta no topo do monte Toledo
+//
+//
+//
+//
+import game.struct.Gurillo;
